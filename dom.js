138e34ecf19db55ef6162ec02f8506b775aa3dd2
unknown <Sergio.Krapchini@yandex.ru>
2022-12-20 16:58:05 +0300

    обновление функции добавления котов

--- dom.js
+++ dom.js
@@ -1,18 +1,37 @@
-const newRateOne = document.querySelector('#budget_one');
-console.log(newRateOne.innerHTML);
+const newRateOne = document.querySelector('#rating');
+    //console.log(newRateOne.innerHTML);
 
-for(let i = 0; i < 11; i ++) {
+for(let i = 0; i <= 10; i ++) {
     let options = document.createElement('option');
     options.value = i; 
     options.innerHTML = i;
     newRateOne.append(options);
 }
 
-const newRateTwo = document.querySelector('#budget_two');
+const newRateTwo = document.querySelector('#age_cat');
 
-for(let i = 0; i < 26; i++) {
+for(let i = 0; i <= 25; i++) {
     let newOptions = document.createElement('option');
     newOptions.value = i;
     newOptions.innerHTML = i;
     newRateTwo.append(newOptions);
+}
+
+//модалка
+const newRateOneModal = document.querySelector('#modal_rating_cat');
+
+for(let i = 0; i <= 10; i ++) {
+    let options = document.createElement('option');
+    options.value = i; 
+    options.innerHTML = i;
+    newRateOneModal.append(options);
+}
+
+const newRateTwoModal = document.querySelector('#modal_age_cat');
+
+for(let i = 0; i <= 25; i++) {
+    let newOptions = document.createElement('option');
+    newOptions.value = i;
+    newOptions.innerHTML = i;
+    newRateTwoModal.append(newOptions);
 }
--- indexJS.js
+++ indexJS.js
@@ -1,8 +1,10 @@
 const $cardCats = document.querySelector('[data-btnDel]')
+const $createCatForm = document.forms.createCatForm
 
 const ACTION = {
     DETAIL: 'detail',
-    DELETE: 'delete'
+    DELETE: 'delete',
+    CHANGE: 'change'
 }
 
 const getCardsHTML = function(cat) {
@@ -12,23 +14,25 @@ const getCardsHTML = function(cat) {
                 <img src="${cat.image}" alt="Нужно потерпеть">
                 <h1>"${cat.name}"</h1>
                 <p>${cat.description}</p>
-                <button data-action="${ACTION.DETAIL}" class="btn">Подробно</button>
+                <button data-action="${ACTION.DETAIL}" class="btn__info">Подробно</button>
+                <button data-action="${ACTION.CHANGE}" class="btn__change"> Изменить</button>
                 <button data-action="${ACTION.DELETE}" class="btn__delete">Удалить</button>
             </div>  
         </div>        
     `
 }
-                                                               // fetch - ассинхронный метод, который возвращает промис. Если возвращ. промис, то мы
+                                                                // fetch - ассинхронный метод, который возвращает промис. Если возвращ. промис, то мы
 fetch('https://cats.petiteweb.dev/api/single/SergioJS-ONE/show/')// можем воспользоваться then
     .then((ref) => ref.json()) //ответ приходит в 2 этапа: 1 мы получаем заголовки и статусы. Нас интересует тело ответа, которое мы получем от сервера 
     .then((data) => {         // чтобы его получить, мы обратились ref к json(). Но json() возвращает промис, соотв. нужно обр. к еще одному .then
     
     $cardCats.insertAdjacentHTML('afterbegin', data.map(cat => getCardsHTML(cat)).join(''))
-    console.log({data})
+    // console.log({data})
 })                         
 
+//добавление на кнопки удаления  
 $cardCats.addEventListener('click', (e) => {
-    if(e.target.dataset.action == ACTION.DELETE) {
+    if(e.target.dataset.action === ACTION.DELETE) {
         console.log(e.target);
 
         const $cardCatsId = e.target.closest('[data-cat-id]')
@@ -45,4 +49,36 @@ $cardCats.addEventListener('click', (e) => {
             }
         })
     }  
+})
+
+//дофление функции создания новой карточки кота
+$createCatForm.addEventListener('submit', (e) => {
+    e.preventDefault() // preventDefault - отмена поведение формы по умолчанию
+    
+    let formDataObject = Object.fromEntries(new FormData(e.target).entries())
+
+    formDataObject = {
+        ...formDataObject,
+        id: +formDataObject.id,
+        rate: +formDataObject.rate,
+        age: +formDataObject.age,
+        favorite: !!formDataObject.favorite
+    }
+
+    if(formDataObject.id == '') {
+        alert('Введите обязательные данные')
+    }
+    
+    fetch('https://cats.petiteweb.dev/api/single/SergioJS-ONE/add/', {
+        method: 'POST',
+        headers: { 
+            'Content-Type': 'application/json' // Content-Type - этим заголовком вы сообщ. серверу, что мы отсыл. ему данные в формате JSON
+        },//'Content-Type': 'application/json' - отображ искл. так, это пропис. в спецификации JS
+        body: JSON.stringify(formDataObject) //объект фн. и тд передать невозможно. По этому мы должны преобразовать в строку
+    }).then((res) => {
+        if (res.status === 200) {
+            return $cardCats.insertAdjacentHTML('afterbegin', getCardsHTML(formDataObject))
+        }
+        throw Error('Ошибка, добавить картоку не получилось')
+    }).catch(alert)
 })
